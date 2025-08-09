f77c16629c19b516adba16fc34b689060a8f4a9d
Reshan Gomis <reshangomis@gmail.com>
2025-08-09 11:54:04 +0530

    fix: add unit trust balance calculation and formatting in check_data function

--- app/personal_finance/checkData.py
+++ app/personal_finance/checkData.py
@@ -17,6 +17,7 @@ async def check_data(chat_id: int):
     
     net_worth = 0
     my_cse_balance = 0
+    my_unit_trust_balance = 0
     dad_cse_balance = 0
     for idx, row in enumerate(net_worth_records[1:], start=2):  # Skip header, start row index at 2
         if len(row) > 2 and row[1]:  # Check column B
@@ -27,11 +28,14 @@ async def check_data(chat_id: int):
                 dad_cse_balance = row[4]
             elif text == "CSE":
                 my_cse_balance = row[3]
+            elif text == "Unit Trust":
+                my_unit_trust_balance = row[3]
 
 
     formatted_net_worth = f"💰 <b>Net Worth:</b> {net_worth}"
     formatted_dad_cse_balance = f"👨‍👦 <b>Dad's CSE Balance:</b> LKR{dad_cse_balance}" if 'dad_cse_balance' in locals() else ""
-    formatted_my_cse_balance = f"👤 <b>My CSE Balance:</b> {my_cse_balance}" if 'my_cse_balance' in locals() else ""
+    formatted_my_unit_trust_balance = f"👤 <b>My Unit Trust Balance:</b> {my_unit_trust_balance}" if my_unit_trust_balance in locals() else ""
+    formatted_my_cse_balance = f"👤 <b>My CSE Balance:</b> {my_cse_balance}" if my_cse_balance in locals() else ""
 
     my_money_export_sheet = connect_to_google_sheet("Financial Overview", "My Money Export")
     if not my_money_export_sheet:
@@ -63,6 +67,7 @@ async def check_data(chat_id: int):
 
     message += f"\n\n{formatted_net_worth}"
     message += f"\n\n{formatted_my_cse_balance}"
+    message += f"\n\n{formatted_my_unit_trust_balance}"
     message += f"\n\n{formatted_dad_cse_balance}"
     message += "\n----------------------------------------------------------------\n"
 
