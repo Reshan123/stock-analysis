b3a0dd012affd386efe5aa1dfbd08f2b64cf37b2
Reshan Gomis <reshangomis@gmail.com>
2025-08-27 21:24:22 +0530

    Update telegram.py

--- app/utils/telegram.py
+++ app/utils/telegram.py
@@ -2,10 +2,16 @@ import requests
 import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
+TELEGRAM_TOKEN_2 = os.getenv("TELEGRAM_BOT_TOKEN_2", "")
 
-def send_telegram_message(chat_id: int, text: str):
+TELEGRAM_API_URL = ""
+
+def send_telegram_message(chat_id: int, text: str, bot_version = 1):
     try:
+        if bot_version == 1:
+            TELEGRAM_API_URL =  f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
+        else:
+            TELEGRAM_API_URL =  f"https://api.telegram.org/bot{TELEGRAM_TOKEN_2}"
         payload = {
         "chat_id": chat_id,
         "text": text,
