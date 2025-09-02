0e7bbc9ca962a4b3455de1ef774091618a7252bf
Reshan Gomis <reshangomis@gmail.com>
2025-09-02 14:24:27 +0530

    testing appending data from excel to financial overview sample tab

--- app/data_pipeline/dataPipeline.py
+++ app/data_pipeline/dataPipeline.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import io
 
+from app.utils.connectToGoogleSheet import connect_to_google_sheet
 from app.utils.connectToGoogleDrive import get_single_drive_file_content
 from app.utils.telegram import send_telegram_message
 
@@ -17,6 +18,27 @@ async def data_pipeline(bot_version = 1):
             
             print("Successfully loaded CSV into DataFrame:")
             print(df.head())
+
+            sheet_name = "Financial Overview"
+            sheet = connect_to_google_sheet(sheet_name, "Sheet17")
+            if not sheet:
+                print(f"Failed to connect to Google Sheet. {sheet}")
+                return
+            try:
+                # --- 3. Convert DataFrame to a list of lists (without header) ---
+                data_to_append = df.values.tolist()
+                
+                # --- 4. Append the data to the worksheet ---
+                sheet.append_rows(data_to_append)
+                
+                print(f"✅ Success! Appended {len(data_to_append)} rows to 'Sheet17'.")
+
+            except gspread.exceptions.APIError as e:
+                print(f"❌ Google API Error: {e}")
+            except Exception as e:
+                print(f"❌ An unexpected error occurred: {e}")
+
+        
     except Exception as e:
         print(f"Error in update_stock_prices: {e}")
         send_telegram_message(
