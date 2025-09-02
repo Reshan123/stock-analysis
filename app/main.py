baf20e06e4191a54348c640c3508c8af369b2ae3
Reshan Gomis <reshangomis@gmail.com>
2025-09-02 10:55:57 +0530

    fix import error

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.company.getCompanyInfo import get_company_info
 from app.personal_finance.checkData import check_data
 from app.company.getCompanyRecommendation import get_stock_recommendation
 from app.sheets_integrations.updateStockPrices import update_stock_prices
+from app.data_pipeline.datPipeline import data_pipeline
 from app.cal_integrations.getCalData import get_cal_data
 from app.utils.telegram import send_telegram_message
 
